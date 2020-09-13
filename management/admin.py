09cedc09a353a5b2436f94f590b5d15ffe83d472
YourAlly <Royettemiranda@YAHOO.COM>
2020-09-13 21:04:25 +0800

    Final Commit for 13/09/2020
    I didn't do much other than do youtube tbh

--- management/admin.py
+++ management/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from .models import User, Tenant, Unit, Residence
+from django.contrib.auth.admin import UserAdmin
+
+admin.site.site_header = 'Admin Panel'
+admin.site.site_title = 'Admin page'
+
+
+class ResidenceAdmin(admin.ModelAdmin):
+    list_display = ('tenant', 'unit', 'date_joined', 'is_active',)
+    search_fields = ('tenant', 'unit',)
+    list_filter = ('is_active',)
+
 
 # Register your models here.
+
+@admin.register(User)
+class CustomUserModelAdmin(UserAdmin):
+    fieldsets = (
+        (None, {
+            "fields": (
+                'username',
+                'password',
+                'first_name',
+                'last_name',
+                'is_tenant',
+                'is_staff',
+                'is_active',
+                'is_superuser',
+                'last_login',
+                'date_joined',
+            ),
+        }),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            "fields": (
+                'username',
+                'password',
+                'first_name',
+                'last_name',
+                'is_tenant',
+            ),
+        }),
+    )
+    
+    list_display = ('username', 'first_name', 'last_name', 'is_tenant')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    list_filter = ('is_tenant',)
+
+admin.site.register(Tenant)
+admin.site.register(Unit)
+admin.site.register(Residence, ResidenceAdmin)
--- management/models.py
+++ management/models.py
@@ -1,17 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
-# Create your models here.
 
+# Create your models here.
 class User(AbstractUser):
-    is_tenant = models.BooleanField(default=False)
+    is_tenant = models.BooleanField(default=False, help_text='Tenant Status')
+
+
+class Unit(models.Model):
+    name = models.CharField(max_length=64)
+    cost = models.IntegerField(default=0)
+    details = models.TextField()
+
+    def __str__(self):
+        return f'{self.name}'
 
 class Tenant(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}' if self.user.first_name or self.user.last_name else self.user.username
+    
+class Residence(models.Model):
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE, related_name='units')
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE, related_name='residents')
     is_active = models.BooleanField(default=True)
     date_joined = models.DateField(default=timezone.now)
     date_left = models.DateField(blank=True, null=True)
 
-class Unit(models.Model):
-    name = models.CharField(max_length=64)
-    details = models.TextField()
